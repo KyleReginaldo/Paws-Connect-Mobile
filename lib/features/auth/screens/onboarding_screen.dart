3542031ce91d1058333220bdc50e7d8f8270a9da
kyle reginaldo <97393306+KyleReginaldo@users.noreply.github.com>
2025-08-23 23:14:34 +0800

    Add onboarding screen and background image
    Introduces OnboardingScreen widget displaying a background image. Adds onboarding_background.jpg to assets and updates pubspec.yaml to include the new image asset.

---
+++ lib/features/auth/screens/onboarding_screen.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        'assets/images/onboarding_background.png',
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
